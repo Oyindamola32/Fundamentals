b2f5a1886b174c1600f4059277159984e57aea4a
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-09 10:10:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let js = "amazing";
+//if (js === "amazing") alert("Javascript is FUN!");
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jones')
+console.log(23)
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
