348cd4e6e2351bae5dd845830f9c5b75a3417ca0
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-09 10:42:43 +0100

    modify script

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ let firstName = "Jonas";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
+
+
+//Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programming";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programming";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
