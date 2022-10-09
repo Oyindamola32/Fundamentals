aac05f275e7b88e484f9ed6a1a395237cae60e92
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-09 11:07:07 +0100

    modify script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 //if (js === "amazing") alert("Javascript is FUN!");
 console.log(40 + 8 + 23 - 10);
 
@@ -25,3 +25,28 @@ let job1 = "Programming";
 let job2 = "Teacher";
 
 console.log(myFirstJob);
+*/
+
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof JavascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jones');
+
+//dynamic typing
+JavascriptIsFun = 'YES!';
+console.log(typeof JavascriptIsFun);
+
+
+//undefined
+let year;
+console.log(year);
+console.log(typeof year)
+
+
+year = 1999;
+console.log(typeof year);
+
+console.log(typeof null)
